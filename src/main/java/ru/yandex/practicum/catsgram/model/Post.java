e2dc0e5baa3ecf78341af20e245ca41e528922e1
Mariia Postnova <dev.nuggets@yandex.com>
2025-11-26 15:06:48 +0200

    add model package and put User, Post and Image classes in it

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+import java.time.Instant;
+
+@Data
+public class Post {
+    private Long id;
+    private long authorId;
+    private String description;
+    private Instant postDate;
+}

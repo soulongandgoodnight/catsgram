e0f0181a73aceb34a5d7bd97d1f9dadd82c42106
Mariia Postnova <dev.nuggets@yandex.com>
2025-11-26 18:41:48 +0200

    add EqualsAndHashCode lombok to Image,Post and User classes

--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.catsgram.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import java.time.Instant;
 
 @Data
+@EqualsAndHashCode(of = "id")
 public class Post {
     private Long id;
     private long authorId;
--- src/main/java/ru/yandex/practicum/catsgram/model/User.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.catsgram.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import java.time.Instant;
 
 @Data
+@EqualsAndHashCode(of = "email")
 public class User {
     private Long id;
     private String username;

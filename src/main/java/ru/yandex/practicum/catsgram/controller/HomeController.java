8a2cac56d7c82dbffeeffb17c7729e83950707df
Mariia Postnova <dev.nuggets@yandex.com>
2025-11-26 21:26:20 +0200

    add annotation GetMapping to HomeController

--- src/main/java/ru/yandex/practicum/catsgram/controller/HomeController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/HomeController.java
@@ -1,12 +1,14 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class HomeController {
 
-    @RequestMapping("/home")
+    @GetMapping
     public String homePage() {
         return "<h1>Приветствуем вас в приложении Котограм<h1>";
     }

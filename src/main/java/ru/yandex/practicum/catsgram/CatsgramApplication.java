0f5bf46e9bcc8aaa5b4126957ab9050cc8317eac
Mariia Postnova <dev.nuggets@yandex.com>
2025-11-26 20:04:46 +0200

    add SpringBoot annotation with run to main

--- src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
+++ src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
@@ -3,11 +3,17 @@ package ru.yandex.practicum.catsgram;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.util.Map;
 import java.util.Scanner;
 
+@SpringBootApplication
 public class CatsgramApplication {
     public static void main(String[] args) {
+        SpringApplication.run(CatsgramApplication.class, args);
+
         final Gson gson = new Gson();
         final Scanner scanner = new Scanner(System.in);
         System.out.print("Введите JSON => ");
